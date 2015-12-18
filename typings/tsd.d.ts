c1f3fe974f83d695d36f61bd4a834b39e829a834
filow <lirenhai427@gmail.com>
2015-12-18 10:13:53 +0800

    Add d.ts file to project

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="async/async.d.ts" />
+/// <reference path="cli-color/cli-color.d.ts" />
+/// <reference path="cheerio/cheerio.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="superagent/superagent.d.ts" />
